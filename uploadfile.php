e2c47fe4992ed92c3474ff38d36e386f17adff27
hegy5998 <hegy5998@gmail.com>
2015-07-25 17:24:11 +0800

    2015.7.25
    新增path參數
    用來判斷該目錄是否存在
    如果不存在則創建

--- uploadfile.php
+++ uploadfile.php
@@ -1,7 +1,12 @@
 <?php
 
 	$root = $_POST['root'];	
+	$path = $_POST['path'];
 
+    if(!file_exists($path))
+    {
+    	mkdir($path,'0777'); //建立資料夾!!!!
+    }
     if($_FILES['attachment']['tmp_name']){		
         move_uploaded_file($_FILES['attachment']['tmp_name'],$root);
         echo "Upload files success!";
